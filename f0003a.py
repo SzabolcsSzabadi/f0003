18bf7431af04625e6bb9026e329437092fce2c2c
SzabolcsSzabadi <55064779+SzabolcsSzabadi@users.noreply.github.com>
2020-10-15 13:12:56 +0200

    Add files via upload

---
+++ f0003a.py
@@ -0,0 +1,8 @@
+#Írj programot, amely külön-külön megkérdi a vezeték- és a keresztneved,
+# majd kiírja őket egy mondatban, pl: A te neved Kovács Boldizsár. (A pontot se felejtsd el!)
+
+vezeteknev = input("Mi a Vezetéknevem?:")
+keresztnev = input("Mi a Keresztnevem?:")
+
+print("A te neved " + vezeteknev + ' ' + keresztnev + "!")
+
---
+++ f0003b.py
@@ -0,0 +1,9 @@
+#Bővítsd az előző programot úgy, hogy a kiírás előtt kérdezze meg a születési évedet és a csillagjegyedet,
+#és az előző feladatban megadott mondat után ezt is közölje veled.
+
+vezeteknev = input("Mi a Vezetéknevem?:")
+keresztnev = input("Mi a Keresztnevem?:")
+szuletesiido = input("Mikor születtem?:")
+
+print("A te neved " + vezeteknev + ' ' + keresztnev + "!")
+print("Te ekkora születtél:" + szuletesiido)
